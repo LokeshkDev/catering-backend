dc01bc694f0275682a29c885f70e67c89f225326
lokeshk2492 <lokeshk2492@gmail.com>
2025-11-20 10:25:32 +0530

    Fix missing route file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-import quotationRoutes from "./routes/quotationroutes.js";
+import quotationRoutes from "./routes/quotationRoutes.js";
 
 const app = express();
 
